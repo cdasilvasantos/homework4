2610395919058723268adb1ce634b25c87496790
Chiara daSilva Santos <cd58@njit.edu>
2024-02-20 16:18:42 -0500

    added command

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,19 @@
-# conftest.py
-import pytest
-from decimal import Decimal
+"""
+Module docstring: conftest.py
+"""
+
+from decimal import Decimal  # pylint: disable=wrong-import-order
+import pytest  # pylint: disable=wrong-import-order disable=unused-import
 from faker import Faker
 from calculator.operations import add, subtract, multiply, divide
 
 fake = Faker()
 
+
 def generate_test_data(num_records):
+    """
+    Generate test data
+    """
     # Define operation mappings for both Calculator and Calculation tests
     operation_mappings = {
         'add': add,
@@ -20,25 +27,33 @@ def generate_test_data(num_records):
         b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
-        
+
         # Ensure b is not zero for divide operation to prevent division by zero in expected calculation
-        if operation_func == divide:
+        if operation_func is divide:
             b = Decimal('1') if b == Decimal('0') else b
-        
+
         try:
-            if operation_func == divide and b == Decimal('0'):
+            if operation_func is divide and b == Decimal('0'):
                 expected = "ZeroDivisionError"
             else:
                 expected = operation_func(a, b)
         except ZeroDivisionError:
             expected = "ZeroDivisionError"
-        
+
         yield a, b, operation_name, operation_func, expected
 
+
 def pytest_addoption(parser):
+    """
+    Add option for number of records
+    """
     parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
 
+
 def pytest_generate_tests(metafunc):
+    """
+    Generate tests dynamically
+    """
     # Check if the test is expecting any of the dynamically generated fixtures
     if {"a", "b", "expected"}.intersection(set(metafunc.fixturenames)):
         num_records = metafunc.config.getoption("num_records")
